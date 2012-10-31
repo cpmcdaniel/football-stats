00c10d7b956c6e21a646730e773e640c974d226e
Craig McDaniel <cpmcdaniel@gmail.com>
2012-10-30 20:22:38 -0400

    Adding imports to the user namespace for the dev profile

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,13 @@
 (ns user
   (:require [clj-webdriver.taxi :as taxi]
-            [clj-http.client :as http])
+            [clj-http.client :as http]
+            football-stats.nfldotcom.storage-test
+            football-stats.nfldotcom.api-test)
   (:use clojure.tools.namespace.repl
         [clj-http.util :only [url-encode]]
-        clojure.test))
+        clojure.test
+        [datomic.api :only [db q] :as d]
+        football-stats.nfldotcom.storage
+        football-stats.nfldotcom.api
+        [football-stats.nfldotcom.schema :as schema]))
 
